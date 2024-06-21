8aa895b0451e347182c2a570d1b69d6db235ba7c
Galib Mia <gm71bd@gmail.com>
2024-06-21 12:27:26 +0600

    News data added and load data using ID, category and all

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app =express();
+const app = express();
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+
+const news = require('./data/news.json');
 
 const cors = require('cors');
 
@@ -14,6 +16,30 @@ app.get('/', (re, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+// Get all news
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+// Get specific news using ID
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedID = news.find(n => n._id === id);
+    res.send(selectedID);
+})
+
+// Get News using category
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '0') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
 })
 
 app.listen(port, () => {
